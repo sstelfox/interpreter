315c80d815e9323c14cf6e466eff3be37e7e90d6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-08-02 19:46:59 -0500

    Rework the structure and lexer to make the more useful bits easier to access and bring in the complete lexeme

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,20 @@ mod errors {
     use std::error::Error;
     use std::fmt::{self, Display};
 
-    use crate::tokens::TokenSpan;
+    use crate::tokens::Token;
 
     #[derive(Debug, PartialEq)]
     pub enum RIError {
         // This should be a LexicalError and I should ditch the Illegal token
-        InvalidCharacter(TokenSpan),
+        InvalidCharacter(Token),
     }
 
     impl Display for RIError {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             use self::RIError::*;
 
-            match *self {
-                InvalidCharacter(tok_sp) => write!(
-                    f,
-                    "encountered an invalid character {}: {:?}",
-                    tok_sp.location(),
-                    tok_sp.token()
-                ),
+            match self {
+                InvalidCharacter(tok) => write!(f, "encountered an invalid character: {:?}", tok),
             }
         }
     }
@@ -39,122 +34,162 @@ mod errors {
 }
 
 mod tokens {
-    #[derive(Clone, Copy, Debug, PartialEq)]
-    pub enum Token {
-        Integer(i64),
+    #[derive(Debug, PartialEq)]
+    pub enum Literal {
+        Identifier(String),
+        Number(f64),
+        Text(String),
+    }
 
+    #[derive(Debug, PartialEq)]
+    pub struct Token {
+        pub token_type: TokenType,
+        pub literal: Option<Literal>,
+        pub location: Option<SourceLocation>,
+    }
+
+    impl Token {
+        pub fn new(tok_t: TokenType) -> Self {
+            Token {
+                token_type: tok_t,
+                literal: None,
+                location: None,
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub enum TokenType {
+        // Single character tokens
+        Comma,
+        Dot,
+        LeftBrace,
         LeftParen,
-        RightParen,
-
-        Division,
         Minus,
-        Multiplication,
         Plus,
+        RightBrace,
+        RightParen,
+        SemiColon,
+        Slash,
+        Star,
+
+        // One or two character tokens
+        Bang,
+        BangEqual,
+        Equal,
+        EqualEqual,
+        Greater,
+        GreaterEqual,
+        Less,
+        LessEqual,
+
+        // Literals (Note: Text is named String in the reference grammar)
+        Identifier,
+        Number,
+        Text,
+
+        // Keywords
+        And,
+        Class,
+        Else,
+        False,
+        For,
+        Fun,
+        If,
+        Nil,
+        Or,
+        Print,
+        Return,
+        Super,
+        This,
+        True,
+        Var,
+        While,
 
         EOF,
-
-        // NOTE: Maybe this shouldn't be a token but instead come out of the tokenizer as an
-        // error... I could make the error message a bit cleaner and avoid a bunch of object
-        // instantiation...
-        Illegal(char),
     }
 
     /// This data structure wraps lex'd tokens with information useful for diagnosing the source of
     /// errors in input programs.
-    ///
-    /// TODO: I should just track character offset from the beginning of the input. It's simpler to
-    /// do on the fly. If an error in the input occurs, the input can be rescanned back to we can
-    /// do the more expensive operation of find the specific line and set of characters that it
-    /// applies to. I'll still want the start and end character offsets though.
-    #[derive(Clone, Copy, Debug, PartialEq)]
-    pub enum TokenSpan {
-        /// Used by the TokenLexer for tests to indicate which of the preprocessed tokens were
-        /// invalid. The token number is zero indexed to match its position in the input array.
-        CountSpan(Token, usize),
-
-        /// Used for single character tokens. The first numeric is the line the token was found on,
-        /// the second is the column.
-        SimpleSpan(Token, usize, usize),
-
-        /// When a token is complex enough that it spans multiple characters this span is used to
-        /// capture the full range of the input consumed. In order the numeric arguments are: line
-        /// number, column start, column end.
-        RangeSpan(Token, usize, usize, usize),
+    #[derive(Debug, PartialEq)]
+    pub enum SourceLocation {
+        Point(usize),
+        Range(usize, usize),
     }
 
-    impl TokenSpan {
-        pub fn location(&self) -> String {
-            use self::TokenSpan::*;
-
-            match *self {
-                CountSpan(_, tok_id) => format!("at token number {}", tok_id),
-                SimpleSpan(_, line, column) => format!("at line number {} column {}", line, column),
-                RangeSpan(_, line, s_col, e_col) => format!(
-                    "at line number {} between columns {} and {}",
-                    line, s_col, e_col
-                ),
-            }
+    impl SourceLocation {
+        // SourceLocation's always start as a single character location, but can be extended to
+        // cover a range.
+        pub fn new(loc: usize) -> Self {
+            SourceLocation::Point(loc)
         }
 
-        pub fn token(&self) -> Token {
-            use self::TokenSpan::*;
+        pub fn extend_to(&mut self, end_pos: usize) {
+            use self::SourceLocation::*;
 
-            match *self {
-                CountSpan(tok, _) => tok,
-                SimpleSpan(tok, _, _) => tok,
-                RangeSpan(tok, _, _, _) => tok,
-            }
+            let start_pos = match *self {
+                Point(start) => start,
+                Range(start, _) => start,
+            };
+
+            // Should I do some kind of validation here? Check that end > start? Nah not for now...
+            *self = Range(start_pos, end_pos);
         }
     }
 }
 
 mod lexer {
+    use std::collections::VecDeque;
     use std::iter::Peekable;
     use std::str::Chars;
 
-    use crate::tokens::{Token, TokenSpan};
+    use crate::tokens::{SourceLocation, Token, TokenType};
 
     pub trait Lexer {
-        fn next_token(&mut self) -> TokenSpan;
+        fn next_token(&mut self) -> Token;
     }
 
     pub struct TokenLexer {
         pos: usize,
-        token_list: Vec<Token>,
+        token_list: VecDeque<Token>,
     }
 
     impl TokenLexer {
         pub fn new(tokens: Vec<Token>) -> Self {
             TokenLexer {
                 pos: 0,
-                token_list: tokens,
+                token_list: VecDeque::from(tokens),
             }
         }
     }
 
     impl Lexer for TokenLexer {
-        fn next_token(&mut self) -> TokenSpan {
-            if self.pos >= self.token_list.len() {
-                return TokenSpan::CountSpan(Token::EOF, self.pos);
+        fn next_token(&mut self) -> Token {
+            if self.token_list.is_empty() {
+                return Token::new(TokenType::EOF);
             }
 
+            let source_loc = SourceLocation::new(self.pos);
             self.pos += 1;
-            TokenSpan::CountSpan(self.token_list[self.pos - 1], self.pos - 1)
+
+            // Grab the next token and label it with the location it was extracted from
+            let mut token = self.token_list.pop_front().unwrap();
+            token.location = Some(source_loc);
+
+            token
         }
     }
 
     pub struct InputLexer<'a> {
-        column: usize,
-        line: usize,
         input: Peekable<Chars<'a>>,
+        offset: usize,
     }
 
     impl<'a> InputLexer<'a> {
         pub fn new(input: &'a str) -> Self {
             InputLexer {
-                column: 1,
-                line: 1,
                 input: input.chars().peekable(),
+                offset: 0,
             }
         }
 
@@ -163,42 +198,8 @@ mod lexer {
         }
 
         fn read_char(&mut self) -> Option<char> {
-            self.column += 1;
+            self.offset += 1;
             self.input.next()
-        }
-
-        fn read_numeric(&mut self, first_ch: char) -> TokenSpan {
-            if !first_ch.is_numeric() {
-                // This should never happen and indicates a bug in the caller
-                panic!("a non-numeric character '{}' was provided to read_numeric (presumably at line {}, column {})", first_ch, self.line, self.column - 1);
-            }
-
-            let start_column = self.column - 1;
-            let mut numeric_chars = vec![first_ch];
-            loop {
-                match self.peek_char() {
-                    Some(ch) => {
-                        if !ch.is_numeric() {
-                            break;
-                        }
-                        numeric_chars.push(self.read_char().unwrap());
-                    }
-                    None => break,
-                }
-            }
-
-            let end_column = self.column - 1;
-            let num = numeric_chars
-                .into_iter()
-                .collect::<String>()
-                .parse::<i64>()
-                .unwrap();
-
-            if start_column == end_column {
-                TokenSpan::SimpleSpan(Token::Integer(num), self.line, start_column)
-            } else {
-                TokenSpan::RangeSpan(Token::Integer(num), self.line, start_column, end_column)
-            }
         }
 
         fn skip_whitespace(&mut self) {
@@ -207,179 +208,65 @@ mod lexer {
                     break;
                 }
 
-                if c == '\n' {
-                    self.line += 1;
-                    self.column = 0;
-                }
-
                 self.read_char();
             }
         }
     }
 
     impl<'a> Lexer for InputLexer<'a> {
-        fn next_token(&mut self) -> TokenSpan {
+        fn next_token(&mut self) -> Token {
             self.skip_whitespace();
 
-            let current_line = self.line;
-            let current_column = self.column;
-
-            let token = match self.read_char() {
-                Some('-') => Token::Minus,
-                Some('+') => Token::Plus,
-                Some('/') => Token::Division,
-                Some('*') => Token::Multiplication,
-                Some('(') => Token::LeftParen,
-                Some(')') => Token::RightParen,
-                Some(ref ch) => {
-                    if ch.is_numeric() {
-                        return self.read_numeric(*ch);
-                    } else {
-                        Token::Illegal(*ch)
-                    }
+            let source_loc = SourceLocation::new(self.offset);
+            let token_type = match self.read_char() {
+                Some('-') => TokenType::Minus,
+                Some('+') => TokenType::Plus,
+                Some('/') => TokenType::Slash,
+                Some('*') => TokenType::Star,
+                Some('(') => TokenType::LeftParen,
+                Some(')') => TokenType::RightParen,
+                Some(_) => {
+                    // NOTE: Maybe I should bring back the illegal token?
+                    TokenType::Super
                 }
-                None => Token::EOF,
+                None => TokenType::EOF,
             };
 
-            TokenSpan::SimpleSpan(token, current_line, current_column)
+            let mut token = Token::new(token_type);
+            token.location = Some(source_loc);
+            return token;
         }
-    }
-
-    #[test]
-    fn test_token_lexer() {
-        let mut lexer = TokenLexer::new(vec![Token::Integer(45), Token::Plus]);
-
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::CountSpan(Token::Integer(45), 0)
-        );
-        assert_eq!(lexer.next_token(), TokenSpan::CountSpan(Token::Plus, 1));
-        assert_eq!(lexer.next_token(), TokenSpan::CountSpan(Token::EOF, 2));
-    }
-
-    #[test]
-    fn test_input_lexer() {
-        let test_input = "   10 + (4  * 2 - 300) / 8\n3 + 8";
-        let mut lexer = InputLexer::new(test_input);
-
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::RangeSpan(Token::Integer(10), 1, 4, 5)
-        );
-        assert_eq!(lexer.next_token(), TokenSpan::SimpleSpan(Token::Plus, 1, 7));
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::LeftParen, 1, 9)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Integer(4), 1, 10)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Multiplication, 1, 13)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Integer(2), 1, 15)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Minus, 1, 17)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::RangeSpan(Token::Integer(300), 1, 19, 21)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::RightParen, 1, 22)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Division, 1, 24)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Integer(8), 1, 26)
-        );
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Integer(3), 2, 1)
-        );
-        assert_eq!(lexer.next_token(), TokenSpan::SimpleSpan(Token::Plus, 2, 3));
-        assert_eq!(
-            lexer.next_token(),
-            TokenSpan::SimpleSpan(Token::Integer(8), 2, 5)
-        );
     }
 }
 
 mod parser {
-    use crate::errors::RIError;
-    use crate::lexer::Lexer;
-    use crate::tokens::{Token, TokenSpan};
-
-    pub struct Parser {
-        current_token: TokenSpan,
-        lexer: Box<dyn Lexer>,
-    }
-
-    impl Parser {
-        fn advance(&mut self) {
-            self.current_token = self.lexer.next_token();
-        }
-
-        pub fn construct_ast(&mut self) -> Result<TokenSpan, RIError> {
-            match self.current_token.token() {
-                Token::EOF => Ok(self.current_token),
-                Token::Illegal(_) => Err(RIError::InvalidCharacter(self.current_token)),
-                _ => {
-                    self.advance();
-                    Ok(self.current_token)
-                }
-            }
-        }
-
-        pub fn new(mut lexer: Box<dyn Lexer>) -> Self {
-            let initial_token = lexer.next_token();
-
-            Parser {
-                current_token: initial_token,
-                lexer: lexer,
-            }
-        }
-    }
-
-    #[test]
-    fn test_parser_construction() {
-        let lexer = super::lexer::TokenLexer::new(vec![Token::EOF]);
-        let mut parser = Parser::new(Box::new(lexer));
-
-        assert_eq!(parser.current_token.token(), Token::EOF);
-        assert_eq!(parser.construct_ast().unwrap().token(), Token::EOF);
-    }
-
-    #[test]
-    fn test_illegal_error() {
-        let lexer = super::lexer::TokenLexer::new(vec![Token::Illegal(';')]);
-        let mut parser = Parser::new(Box::new(lexer));
-
-        assert_eq!(
-            parser.construct_ast(),
-            Err(RIError::InvalidCharacter(TokenSpan::CountSpan(
-                Token::Illegal(';'),
-                0
-            )))
-        );
-    }
 }
 
-mod repl {
-    use std::io::{self, BufRead, Write};
+mod interpreter {
+    use std::fs::File;
+    use std::io::{self, BufRead, Read, Write};
 
     use crate::lexer::{InputLexer, Lexer};
-    use crate::tokens::Token;
+    use crate::tokens::TokenType;
+
+    pub fn run_file(path: String) {
+        // TODO: error handling and the like this is fast and dirty
+        let mut file = File::open(path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let mut lexer = InputLexer::new(&contents);
+        let mut current_token = lexer.next_token();
+
+        loop {
+            println!("{:?}", current_token);
+            current_token = lexer.next_token();
+
+            if current_token.token_type == TokenType::EOF {
+                break;
+            }
+        }
+    }
 
     pub fn start_repl() {
         let stdin = io::stdin();
@@ -393,10 +280,11 @@ mod repl {
                 .lock()
                 .read_line(&mut line)
                 .expect("unable to read from input");
-            let mut lexer = InputLexer::new(&line);
 
+            let mut lexer = InputLexer::new(&line);
             let mut current_token = lexer.next_token();
-            if current_token.token() == Token::EOF {
+
+            if current_token.token_type == TokenType::EOF {
                 println!("");
                 break;
             }
@@ -405,7 +293,7 @@ mod repl {
                 println!("{:?}", current_token);
                 current_token = lexer.next_token();
 
-                if current_token.token() == Token::EOF {
+                if current_token.token_type == TokenType::EOF {
                     break;
                 }
             }
@@ -436,15 +324,18 @@ fn main() {
     let args = std::env::args();
 
     if args.len() == 1 {
-        repl::start_repl();
+        interpreter::start_repl();
     } else if args.len() == 2 {
-        if Some("-h".to_string()) == std::env::args().nth(2) {
+        let arg = std::env::args().nth(1).unwrap();
+
+        if "-h".to_string() == arg {
             print_usage();
+            std::process::exit(0);
         } else {
-            // TODO: read in the specified file lex / parse / interpret it
-            unimplemented!();
+            interpreter::run_file(arg);
         }
     } else {
         print_usage();
+        std::process::exit(22);
     }
 }
