38552c16fc6fb84b78bae7bb9b8719a17ef7b7b0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-10-08 23:05:00 -0500

    this is an attempt to build the astprinter

--- src/main.rs
+++ src/main.rs
@@ -664,15 +664,15 @@ mod parser {
     use std::fmt::Debug;
 
     pub trait Expression: Debug {
-        fn accept(self, visitor: Box<dyn ExpressionVisitor>);
+        fn accept(&self, visitor: Box<&dyn ExpressionVisitor>);
     }
 
     macro_rules! define_expression {
-        ($type_name:ident($($field_name:ident: $field_type:ty),*).$visit_key:ident) => {
+        ($type_name:ident($($field_name:ident: $field_type:ty),*)) => {
             #[derive(Debug)]
             pub struct $type_name {
                 $(
-                    $field_name: $field_type,
+                    pub $field_name: $field_type,
                 )*
             }
 
@@ -686,12 +686,6 @@ mod parser {
                 }
             }
 
-            impl Expression for $type_name {
-                fn accept(self, visitor: Box<dyn ExpressionVisitor>) {
-                    visitor.$visit_key(self);
-                }
-            }
-
             // Probably going to need to pass a codeblock into the macro for
             // this...
             impl ExpressionVisitor for $type_name {}
@@ -699,16 +693,56 @@ mod parser {
     }
 
     pub trait ExpressionVisitor {
-        fn visit_binary_expression(&self, _expr: BinaryExpression) {}
-        fn visit_grouping_expression(&self, _expr: GroupingExpression) {}
-        fn visit_literal_expression(&self, _expr: LiteralExpression) {}
-        fn visit_unary_expression(&self, _expr: UnaryExpression) {}
+        fn visit_binary_expression(&self, expr: &BinaryExpression) {}
+        fn visit_grouping_expression(&self, expr: &GroupingExpression) {}
+        fn visit_literal_expression(&self, expr: &LiteralExpression) {}
+        fn visit_unary_expression(&self, expr: &UnaryExpression) {}
     }
 
-    define_expression!(BinaryExpression(left: Box<dyn Expression>, operator: Token, right: Box<dyn Expression>).visit_binary_expression);
-    define_expression!(GroupingExpression(expression: Box<dyn Expression>).visit_grouping_expression);
-    define_expression!(LiteralExpression(value: Literal).visit_literal_expression);
-    define_expression!(UnaryExpression(operator: Token, expression: Box<dyn Expression>).visit_unary_expression);
+    define_expression!(BinaryExpression(left: Box<dyn Expression>, operator: Token, right: Box<dyn Expression>));
+    define_expression!(GroupingExpression(expr: Box<dyn Expression>));
+    define_expression!(LiteralExpression(value: Literal));
+    define_expression!(UnaryExpression(operator: Token, right: Box<dyn Expression>));
+
+    struct AstPrinter;
+
+    impl AstPrinter {
+        fn print(&self, expr: Box<&dyn Expression>) {
+            expr.accept(Box::new(self));
+            println!("");
+        }
+    }
+
+    impl ExpressionVisitor for AstPrinter {
+        fn visit_binary_expression(&self, expr: &BinaryExpression) {
+            print!("({:?} ", expr.operator);
+            expr.left.accept(Box::new(self));
+            print!(" ");
+            expr.right.accept(Box::new(self));
+            print!(")");
+        }
+
+        fn visit_grouping_expression(&self, expr: &GroupingExpression) {
+            print!("(group ");
+            expr.expr.accept(Box::new(self));
+            print!(")");
+        }
+
+        fn visit_literal_expression(&self, expr: &LiteralExpression) {
+            match &expr.value {
+                Literal::Identifier(id) => { print!("id[{}]", id); }
+                Literal::Invalid(ch) => { print!("!invalid[{}]!", ch); }
+                Literal::Number(n) => { print!("num[{}]", n); }
+                Literal::Text(txt) => { print!("txt[{}]", txt); }
+            }
+        }
+
+        fn visit_unary_expression(&self, expr: &UnaryExpression) {
+            print!("({:?} ", expr.operator);
+            expr.right.accept(Box::new(self));
+            print!(")");
+        }
+    }
 }
 
 mod interpreter {
